732f87a33827f0a9411365d0288a11466c522509
Mario Moran <mjosuemoran029@gmail.com>
2026-03-06 09:30:12 -0600

    Creacion clase GestionTabla y Libreria POI

---
+++ src/lab6file/GestorTablas.java
@@ -0,0 +1,15 @@
+package lab6file;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import org.apache.poi.xwpf.usermodel.*;
+
+
+public class GestorTablas {
+    
+    
+    
+}
